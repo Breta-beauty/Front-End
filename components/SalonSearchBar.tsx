e52b2d31ce28fbaa099050dd8cdbf919cf1d8c15
Derek <a6520150022@utch.edu.mx>
2023-06-14 19:06:44 -0600

    Fix(Salon searchbar): Fixed delayed searches on searchbar

--- components/SalonSearchBar.tsx
+++ components/SalonSearchBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 const IconPack = require("../public/icons/Icons");
 const Icons = new IconPack();
 export default function SalonSearchBar() {
@@ -12,10 +12,21 @@ export default function SalonSearchBar() {
     type: string;
   }
   const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
   const URL: string = "https://breta-api.up.railway.app/graphql";
   const headers = {
     "content-type": "application/json",
   };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleSearch(inputValue)
+      console.log("User stopped writing");
+    }, 500); 
+    return () => {
+      clearTimeout(timeoutId); 
+    };
+  }, [inputValue]);
 
   const handleSearch = async (input: String) => {
     if (input != "") {
@@ -48,6 +59,10 @@ export default function SalonSearchBar() {
       setSearchResults([]);
     }
   };
+  const handleBlur = () => {
+    console.log("Input deselected");
+    // Perform any actions or call functions here
+  };
 
   return (
     <>
@@ -57,12 +72,17 @@ export default function SalonSearchBar() {
             <Icons.Search />
           </div>
           <input
-            onChange={(e) => handleSearch(e.target.value)}
+            onBlur={() => setSearchResults([])}
+            onChange={(e) => setInputValue(e.target.value)}
             className="w-full px-10 text-sm ring-1 ring-gray-300 rounded-md p-2 focus:outline-0 placeholder:text-sm tracking-wider placeholder:text-gray-500 "
             placeholder="Buscar"
           />
         </label>
-        <div className={`absolute ring-1 ring-gray-300 top-10 w-full flex flex-col bg-white rounded-md p-1 backdrop-blur-2xl ${searchResults.length == 0 && "hidden"}`}>
+        <div
+          className={`absolute ring-1 ring-gray-300 top-10 w-full flex flex-col bg-white rounded-md p-1 backdrop-blur-2xl ${
+            searchResults.length == 0 && "hidden"
+          }`}
+        >
           {searchResults &&
             searchResults.map((result: any, index: number) => {
               return (
